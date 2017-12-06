3c04cc33b4487b9f3a737ad97ecb5f5116b7faff
Ayc0 <benjamin.koltes@gmail.com>
2017-12-06 13:10:09 -0500

    Create 1.py

---
+++ 5/1.py
@@ -0,0 +1,19 @@
+file = open("./io.txt", "r")
+io = [int(line.strip()) for line in file.readlines()]
+file.close()
+
+print(io)
+
+pointer = 0
+loop = True
+i = 1
+
+while loop:
+  nextPointer = pointer + io[pointer]
+  if (nextPointer >= len(io)):
+    loop = False
+    print(i)
+  else:
+    io[pointer] += 1
+    i += 1
+    pointer = nextPointer
